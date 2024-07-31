c26ad142a149db2600f56ca500cf6dcca4b4ee72
mkoutra <koutra98@hotmail.com>
2024-07-31 11:27:48 +0300

    Tidying up the css file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", function() {
+    this.document.querySelector("#navToggleBtn").addEventListener('click', function() {
+        document.querySelector("#toggleMenu").classList.toggle('hidden')
+    })
+})
